268d8e7c5822beda276652a3f82007f6d48fbee2
Nathan Zadoks <nathan@nathan7.eu>
2013-05-27 01:41:57 +0200

    handle being passed a promise too

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var object = require('object')
   , objzip = require('objzip')
+  , isPromise = require('is-promise')
   , whenEveryItem = require('when-every-item')
 
 module.exports =
 function whenEveryKey(obj) {
+  if (isPromise(obj)) return obj.then(whenEveryKey)
   return whenEveryItem(object.values(obj))
     .then(function(values) {
       return objzip(object.keys(obj), values)
